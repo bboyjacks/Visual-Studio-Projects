0807e93be300b54f9fe610376099617b8a69c051
Lester <lgdelacruz92@gmail.com>
2018-05-14 21:11:07 -0700

    Added texture to triangle

--- OpenGL_Prototype_1/Mesh.h
+++ OpenGL_Prototype_1/Mesh.h
@@ -6,13 +6,15 @@
 class Vertex
 {
 public:
-    Vertex(const glm::vec3& pos)
+    Vertex(const glm::vec3& pos, const glm::vec2& texCoord)
     {
         this->pos = pos;
+        this->texCoord = texCoord;
     }
+    glm::vec3 pos;
+    glm::vec2 texCoord;
 protected:
 private:
-    glm::vec3 pos;
 };
 
 class Mesh
@@ -27,6 +29,7 @@ private:
     enum
     {
         POSITION_VB,
+        TEXCOORD_VB,
         NUM_BUFFERS
     };
 
