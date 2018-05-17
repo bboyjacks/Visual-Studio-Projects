4f877d51404c0513e2d4bc0336205a6135d736f1
Lester <lgdelacruz92@gmail.com>
2018-05-17 07:35:07 -0700

    Unnecessary files

--- BaseLib/Include/Mesh.h
+++ BaseLib/Include/Mesh.h
@@ -14,13 +14,6 @@ public:
         this->normal = normal;
     }
 
-    Vertex(const glm::vec3& pos, const glm::vec4& vertexColor, const glm::vec3& normal = glm::vec3(0, 0, 0))
-    {
-      this->pos = pos;
-      this->texCoord = texCoord;
-      this->normal = normal;
-      this->vertexColor = vertexColor;
-    }
     glm::vec3 pos;
     glm::vec2 texCoord;
     glm::vec3 normal;
